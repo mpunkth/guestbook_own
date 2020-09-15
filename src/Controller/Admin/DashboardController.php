1dbb845b2fcb9c11694cd448a0cb7f5e702949eb
Martin Hoelzer <mpunkth@martsite.de>
2020-09-15 11:02:12 +0200

    corrected Namespace in DashboardController

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Controller\Admin\;
+namespace App\Controller\Admin;
 
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
