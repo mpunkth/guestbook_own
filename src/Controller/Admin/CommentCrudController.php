312114aa3466b3dad7de5ad1c2626959982230dd
Martin Hoelzer <mpunkth@martsite.de>
2020-09-15 11:41:37 +0200

    configured fields in comment dashboard form

--- src/Controller/Admin/CommentCrudController.php
+++ src/Controller/Admin/CommentCrudController.php
@@ -4,6 +4,11 @@ namespace App\Controller\Admin;
 
 use App\Entity\Comment;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class CommentCrudController extends AbstractCrudController
 {
@@ -12,14 +17,18 @@ class CommentCrudController extends AbstractCrudController
         return Comment::class;
     }
 
-    /*
+    
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            // IdField::new('id'),
+            TextField::new('author'), 
+            TextEditorField::new('text'),
+            EmailField::new('email'),
+            DateTimeField::new('createdAt'),
+            TextField::new('photoFilename'),
+            AssociationField::new('conference')
         ];
     }
-    */
+    
 }
