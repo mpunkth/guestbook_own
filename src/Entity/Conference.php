4a58aa9668ed847f5ab664ee12054e1bf9761bf2
Martin Hoelzer <mpunkth@martsite.de>
2020-11-10 17:14:09 +0100

    cleanup

--- src/Entity/Conference.php
+++ src/Entity/Conference.php
@@ -2,11 +2,9 @@
 
 namespace App\Entity;
 
-use App\Repository\ConferenceRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
-use PhpParser\Node\Expr\Cast\String_;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 
