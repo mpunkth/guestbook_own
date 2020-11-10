cf664f2bbbcc02a08e14bc0f51a464cdaaebc215
Martin Hoelzer <mpunkth@martsite.de>
2020-11-10 10:51:59 +0100

    Configre redis sessions

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -33,7 +33,7 @@ class ConferenceController extends AbstractController
     /**
      * @Route("/conference/{id}", name="conference")
      */
-    public function show(Request $request, Conference $conference, CommentRepository $commentRepository) 
+    public function show(Request $request, Conference $conference, CommentRepository $commentRepository) : Response
     {
     $offset = max(0, $request->query->getInt('offset', 0));
     $paginator = $commentRepository->getCommentPaginator($conference, $offset);
